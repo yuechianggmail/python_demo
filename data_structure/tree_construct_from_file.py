6be25a5a33aed038f1e3895f68d82e17c545ee87
yuechiang <yuechiang@163.com>
2024-04-21 08:45:34 +0800

    add file of tree_construct_from_file

---
+++ data_structure/tree_construct_from_file.py
@@ -0,0 +1,50 @@
+from io import TextIOWrapper
+import os
+
+
+def construct_node(prev_node: object, f: TextIOWrapper):
+    current_line = f.readline()
+    if not current_line:
+        return
+    node = to_node(current_line)
+    if node['indent'] > prev_node['indent']:  # child
+        node['parent'] = prev_node
+        prev_node['children'].append(node)
+    elif node['indent'] == prev_node['indent']:  # sibling
+        prev_node['parent']['children'].append(node)
+        node['parent'] = prev_node['parent']
+    else:  # super
+        super = prev_node['parent']
+        while super:
+            if super['indent'] == node['indent']:
+                super = super['parent']
+                super['children'].append(node)
+                node['parent'] = super
+                break
+            super = super['parent']
+        if super is None:
+            raise Exception('Invalid tree')
+    construct_node(node, f)
+
+
+def to_node(line: str) -> object:
+    node = {}
+    temp = line.lstrip('- ')
+    indent = len(line) - len(temp)
+    id, name = temp.split(' ')
+    node['id'] = id
+    node['name'] = name
+    node['indent'] = indent
+    node['parent'] = None
+    node['children'] = []
+    return node
+
+
+f = open(os.path.join(os.path.dirname(__file__), "tree_data.txt"), "r")
+
+root_line = f.readline()
+root_node = to_node(root_line)
+construct_node(root_node, f)
+print(root_node)
+
+f.close()
